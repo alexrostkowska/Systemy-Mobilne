53c7279d5e0a7f5a080326a2917ce9a61bd3a15f
alexrostkowska <aleksandra.rostkowskaa@gmail.com>
2022-10-25 13:28:02 +0200

    PS2-add-answerShow

--- app/src/main/java/com/example/quiz_ar/MainActivity.java
+++ app/src/main/java/com/example/quiz_ar/MainActivity.java
@@ -1,17 +1,28 @@
 package com.example.quiz_ar;
 import com.example.quiz_ar.Question;
+
+import android.app.VoiceInteractor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.util.Log;
+import android.content.Intent;
+import android.support.annotation.Nullable;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String KEY_CURRENT_INDEX = "currentIndex";
+    private static final String QUIZ_TAG = "QUIZ_TAG";
+    public static final String KEY_EXTRA_ANSWER = "com.example.quiz_ar.correctAnswer";
+    private static final int REQUEST_CODE_PROMPT = 0;
     private Button trueButton;
     private Button falseButton;
     private Button nextButton;
+    private Button promptButton;
     private TextView questionTextView;
+    private boolean answerWasShown;
     private Question[] questions = new Question[] {
             new Question(R.string.q_sat_testing, true),
             new Question(R.string.q_python, true),
@@ -24,18 +35,60 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(QUIZ_TAG, "Wywolana zostala metoda cyklu zycia: onCreate");
         setContentView(R.layout.activity_main);
+        if(savedInstanceState != null){
+            currentIndex = savedInstanceState.getInt(KEY_CURRENT_INDEX);
+        }
         trueButton= findViewById(R.id.true_button);
         falseButton = findViewById(R.id.false_button);
         nextButton = findViewById(R.id.next_button);
         questionTextView = findViewById(R.id.question_text_view);
+        promptButton = findViewById(R.id.prompt_button);
+
+        promptButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, PromptActivity.class);
+                boolean correctAnswer = questions[currentIndex].isTrueAnswer();
+                intent.putExtra(KEY_EXTRA_ANSWER, correctAnswer);
+                startActivityForResult(intent, REQUEST_CODE_PROMPT);
+            }
+        });
         trueButton.setOnClickListener(view -> checkAnswerCorrectness(true));
         falseButton.setOnClickListener(view -> checkAnswerCorrectness(false));
         nextButton.setOnClickListener(view -> {
             currentIndex = (currentIndex+1)%questions.length;
+            answerWasShown = false;
             setNextQuestion();
         });
         setNextQuestion();
+    }
+    protected void onSaveInstanceState (Bundle outState){
+        super.onSaveInstanceState(outState);
+        Log.d(QUIZ_TAG, "Wywolana zostala metoda: onSaveInstanceState");
+        outState.putInt(KEY_CURRENT_INDEX, currentIndex);
+    }
+    private static final String TAG = "MainActivity";
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG,"Start!");
+    }
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG,"Resume!");
+    }
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "Pause!");
+    }
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG,"Stop!");
+    }
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG,"Destroy!");
     }
     private void setNextQuestion(){
         questionTextView.setText(questions[currentIndex].getQuestionId());
@@ -43,13 +96,32 @@ public class MainActivity extends AppCompatActivity {
     private void checkAnswerCorrectness(boolean userAnswer){
         boolean correctAnswer = questions[currentIndex].isTrueAnswer();
         int resultMessageId = 0;
-        if (userAnswer == correctAnswer){
-            resultMessageId = R.string.correct_answer;
+        if(answerWasShown){
+            resultMessageId = R.string.answer_was_shown;
         }
-        else{
-            resultMessageId = R.string.incorrect_answer;
+        else {
+            if(userAnswer == correctAnswer) {
+                resultMessageId = R.string.correct_answer;
+            }
+            else {
+                resultMessageId = R.string.incorrect_answer;
+            }
         }
         Toast.makeText(this, resultMessageId, Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode != RESULT_OK) {
+            return;
+        }
+
+        if(requestCode == REQUEST_CODE_PROMPT) {
+            if(data == null) {
+                return;
+            }
+            answerWasShown = data.getBooleanExtra(PromptActivity.KEY_EXTRA_ANSWER_SHOWN, false);
+        }
     }
 
 }
